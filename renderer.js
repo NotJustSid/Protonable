fb6002ff8b275656bd1bb881fe087ae39d560c43
Sid <bhowmicksiddhanth11@gmail.com>
2018-11-26 00:55:02 +0530

    New Changes, File Explorer Shows and Gave up idea of Terminal

---
+++ renderer.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+function readDir(path) {
+    fs.readdir(path, (err, files) => {
+        'use strict';
+        if (err) throw  err;
+        for (let file of files) {
+           fs.stat(path.slice(0,-1)+file,(err,stats)=>{
+               if(err) throw err;
+              
+               if(stats.isDirectory()){
+               
+           document.getElementById('file-list').innerHTML += `<li><i class="fa fa-folder-open dir"></i> ${file}</li>`;
+               }
+             
+                else {
+                  document.getElementById('file-list').innerHTML += `<li><i class="fa fa-file dir"></i> ${file}</li>`;
+                      }
+           });
+        }
+    });
+}
+
+(function (){
+readDir('.');
+})();
