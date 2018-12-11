00b65d5c8cae6608dbd3c6cc7ba8958a7010ba6e
Sid <techtotallyunique@gmail.com>
2018-12-11 20:38:50 +0530

    File Explorer Finally Works
    The File Explorer After Several Attempts at it, has been made to work by me! New Updates on it Gonna COME SOON!

--- renderer.js
+++ renderer.js
@@ -1,6 +1,9 @@
+resolve = require('path').resolve;
+
 function readDir(path2) {
     fs.readdir(path2, (err, files) => {
         'use strict';
+        document.getElementById('file-list').innerHTML = "";
         if (err) throw  err;
         for (let file of files) {
            fs.stat(path2.slice(0,-1)+file,(err,stats)=>{
@@ -8,7 +11,7 @@ function readDir(path2) {
                
                if(stats.isDirectory()){
                
-           document.getElementById('file-list').innerHTML += `<li><i class="fa fa-folder-open dir"></i> ${file}</li>`;
+           document.getElementById('file-list').innerHTML += `<li id="${resolve(path2) + '/' + file + '//'}" onmousedown="readDir($(this).attr('id'))"><i class="fa fa-folder-open dir"></i> ${file}</li>`;
                }
              
                 else {
