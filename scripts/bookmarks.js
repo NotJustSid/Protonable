fd1a8f77fa4005d8c6096174a57f00c7b0916248
Sid <techtotallyunique@gmail.com>
2018-12-31 13:28:32 +0530

    BookMarks Totally Functionable - 1
    BookMarks are now totally working, except the fact of saving them after window close. Gonna add that afterwards...

--- scripts/bookmarks.js
+++ scripts/bookmarks.js
@@ -4,8 +4,15 @@ function readBook(){
     for(var i = 0; i<linesBooked.length; i++){
         Book.innerHTML+=  `<a class="list-group-item list-group-item-action" style="user-select:none;" ondblclick="editor.setCursor(${linesBooked[i]-1}, 0)">
         Line:&nbsp;&nbsp;&nbsp;
-        <span class="badge badge-primary badge-pill">${linesBooked[i]}</span><br>
+        <span class="badge badge-primary badge-pill">${linesBooked[i]}</span>
+        <button type="button" class="close" onclick="deleteBookMark(${linesBooked[i]})">&times;</button>
+        <br>
         ${editor.getLine(linesBooked[i]-1).substring(0, 36)}
         </a>`
     }
+}
+function deleteBookMark(line){
+    var index = linesBooked.indexOf(line);
+    if (index !== -1) linesBooked.splice(index, 1);
+    readBook();
 }
