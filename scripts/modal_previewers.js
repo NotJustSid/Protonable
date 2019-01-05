6ed65496f0c3ea1c3c455d0f7202bead745778f9
Sid <techtotallyunique@gmail.com>
2019-01-05 13:08:52 +0530

    Problem With Minimap Solved

--- scripts/modal_previewers.js
+++ scripts/modal_previewers.js
@@ -9,7 +9,7 @@
 
 function showFind(){
    var WidthDrag  = window.innerWidth;
-   var HeightDrag = window.innerHeight - 360;
+   var HeightDrag = window.innerHeight-360;
     $('#Find').modal('show'); 
     $('.modal-backdrop').removeClass("modal-backdrop"); 
     $('#Find').draggable({
@@ -22,30 +22,39 @@ function showFind(){
      
     }
 function showReplace(){
+   var WidthDrag  = window.innerWidth;
+   var HeightDrag = window.innerHeight-560;
     $('#Replace').modal('show');
     $('.modal-backdrop').removeClass("modal-backdrop");  
     $('#Replace').draggable({
     handle: ".modal-header",
+    containment: [0,0,WidthDrag,HeightDrag],
     start: function(event, ui){
     $(ui.helper).css('width', `${ $(event.target).width() }px`);
     }
  });
     }
 function showTheme(){
+   var WidthDrag  = window.innerWidth;
+   var HeightDrag = window.innerHeight-760;
     $('#Theme').modal('show');
     $('.modal-backdrop').removeClass("modal-backdrop");  
     $('#Theme').draggable({
         handle: ".modal-header",
+        containment: [0,0,WidthDrag,HeightDrag],
         start: function(event, ui){
            $(ui.helper).css('width', `${ $(event.target).width() }px`);
         }
      });
                     }
 function showFont(){
+   var WidthDrag  = window.innerWidth;
+   var HeightDrag = window.innerHeight-360;
     $('#FontSet').modal('show');
     $('.modal-backdrop').removeClass("modal-backdrop");  
     $('#FontSet').draggable({
         handle: ".modal-header",
+        containment: [0,0,WidthDrag,HeightDrag],
         start: function(event, ui){
            $(ui.helper).css('width', `${ $(event.target).width() }px`);
         }
@@ -55,11 +64,13 @@ function showMode(){
      $('#ModeModal').modal('show');
 }
 function line_col(){
-    
+   var WidthDrag  = window.innerWidth;
+   var HeightDrag = window.innerHeight-360;
     $('#cursorPos').modal('show');
     $('.modal-backdrop').removeClass("modal-backdrop");
     $('#cursorPos').draggable({
         handle: ".modal-header",
+        containment: [0,0,WidthDrag,HeightDrag],
         start: function(event, ui){
            $(ui.helper).css('width', `${ $(event.target).width() }px`);
         }
@@ -69,10 +80,13 @@ function newproject(){
     $('#Propath').modal('show');
 }
 function showUI(){
+   var WidthDrag  = window.innerWidth;
+   var HeightDrag = window.innerHeight-360;
     $('#UI').modal('show');
     $('.modal-backdrop').removeClass("modal-backdrop");
     $('#UI').draggable({
         handle: ".modal-header",
+        containment: [0,0,WidthDrag,HeightDrag],
         start: function(event, ui){
            $(ui.helper).css('width', `${ $(event.target).width() }px`);
         }
@@ -82,10 +96,13 @@ function showHelp(){
     $('#Help').modal('show'); 
 }
 function showBook(){
+   var WidthDrag  = window.innerWidth;
+   var HeightDrag = window.innerHeight-360;
     $('#BookModal').modal('show');
     $('.modal-backdrop').removeClass("modal-backdrop");
     $('#BookModal').draggable({
         handle: ".modal-header",
+        containment: [0,0,WidthDrag,HeightDrag],
         start: function(event, ui){
            $(ui.helper).css('width', `${ $(event.target).width() }px`);
         }
