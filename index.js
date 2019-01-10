f042579a204cdb6fa6395516dbc6263cf3030bbc
Sid <techtotallyunique@gmail.com>
2019-01-11 02:06:17 +0530

    Playing With Updater
    Part 2: Failed with the first one. Forgot to add some code...

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 //Modules
 
 const {app, BrowserWindow, globalShortcut} = require('electron')
-require('update-electron-app')()
 const url = require('url');
 const path = require('path');
+const log = require('electron-log');
+const {autoUpdater} = require("electron-updater");
+
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+log.info('App starting...');
 
 
 let mainWindow;
 
+function sendStatusToWindow(text) {
+  log.info(text);
+  mainWindow.webContents.send('message', text);
+}
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -47,7 +56,31 @@ function createWindow() {
   });
 }
 
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
+})
+autoUpdater.on('update-available', (info) => {
+  sendStatusToWindow('Update available.');
+})
+autoUpdater.on('update-not-available', (info) => {
+  sendStatusToWindow('Update not available.');
+})
+autoUpdater.on('error', (err) => {
+  sendStatusToWindow('Error in auto-updater. ' + err);
+})
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  sendStatusToWindow(log_message);
+})
+autoUpdater.on('update-downloaded', (info) => {
+  sendStatusToWindow('Update downloaded');
+});
 
 
 app.on('ready', createWindow);
 
+app.on('ready', function()  {
+  autoUpdater.checkForUpdatesAndNotify();
+});
